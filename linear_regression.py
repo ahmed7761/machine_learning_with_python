4480ca575bc43731db3c639d8abab8491963b997
sharif ahmed <sharif@namespaceit.com>
2021-02-14 16:02:23 +0600

    fixed linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -6,13 +6,14 @@ import pandas as pd
 # Importing the dataset 
 dataset = pd.read_csv('Salary_Data.csv') 
 
-X = dataset[['YearsExperience']] 
+'''
+X = dataset[['YearsExperience']]
 y = dataset[['Salary']]
 
 '''
-X = dataset.iloc[:, 0].values 
+X = dataset.iloc[:, 0:1].values
 y = dataset.iloc[:, 1].values
-'''
+
 # Splitting the dataset into the Training set and Test set 
 from sklearn.model_selection import train_test_split 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
